2fa7b66a27de8bff2203cc9b77aa9c5b13bce0ec
Joe Walsh <joewalsh@users.noreply.github.com>
2020-08-01 13:32:00 -0400

    Update Line.swift

--- Toynbee/Model/Line.swift
+++ Toynbee/Model/Line.swift
@@ -1,36 +1,36 @@
 enum Line: String {
-    case air
-    case che
-    case chw
-    case lan
-    case med
-    case fox
-    case nor
-    case pao
-    case cyn
-    case tre
-    case war
-    case wil
-    case wtr
+    case AIR
+    case CHE
+    case CHW
+    case LAN
+    case MED
+    case FOX
+    case NOR
+    case PAO
+    case CYN
+    case TRE
+    case WAR
+    case WIL
+    case WTR
     
     var code: String {
-        return rawValue.uppercased()
+        return rawValue
     }
-    
+
     private static let byName: [String: Line] = [
-        "airport": .air,
-        "chestnut hill east": .che,
-        "chestnut hill west": .chw,
-        "lansdale/doylestown": .lan,
-        "media/elwyn": .med,
-        "fox chase": .fox,
-        "manayunk/norristown": .nor,
-        "paoli/thorndale": .pao,
-        "cynwyd": .cyn,
-        "trenton": .tre,
-        "warminster": .war,
-        "wilmington/newark": .wil,
-        "west trenton": .wtr,
+        "airport": .AIR,
+        "chestnut hill east": .CHE,
+        "chestnut hill west": .CHW,
+        "lansdale/doylestown": .LAN,
+        "media/elwyn": .MED,
+        "fox chase": .FOX,
+        "manayunk/norristown": .NOR,
+        "paoli/thorndale": .PAO,
+        "cynwyd": .CYN,
+        "trenton": .TRE,
+        "warminster": .WAR,
+        "wilmington/newark": .WIL,
+        "west trenton": .WTR,
     ]
     
     public static func with(name: String) -> Line? {
